9f8b4c7d7b3c5f1dbc61f6e03eb94bb329cb11f5
kkaan <kankeean@gmail.com>
2024-05-31 20:24:28 +1000

    added intrinsic pr corrections to plotting.

--- src/plotsforpresentation/summaryboxplots.py
+++ src/plotsforpresentation/summaryboxplots.py
@@ -3,35 +3,27 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the data
-file_path = (r"P:\02_QA Equipment\02_ArcCheck\05_Commissoning\03_NROAC\Dose Rate Dependence Fix\Test on "
-             r"script\ACResultsPowerQuerySummary.csv")
-data = pd.read_csv(file_path)
-
-# Combine the split columns for easier manipulation
-data['Measured: Diode Array Calc shift (mm)'] = data['Measured: Diode Array'] + ' ' + data['Calc shift (mm)']
-
-# Drop the original split columns
-data_corrected = data.drop(columns=['Measured: Diode Array', 'Calc shift (mm)'])
+file_path = r"P:\02_QA Equipment\02_ArcCheck\05_Commissoning\03_NROAC\Dose Rate Dependence Fix\Test on script\ACResultsPowerQuerySummary.txt"
+data = pd.read_csv(file_path, sep='\t')
 
 # Melt the data
-data_melted = data_corrected.melt(id_vars=['Measured: Diode Array Calc shift (mm)', 'Correction'],
-                                  value_vars=['3%, 3mm', '2%, 3mm', '1%, 3mm'],
-                                  var_name='Percentage', value_name='Value')
+data_melted = data.melt(id_vars=['Plan Name', '(mm)', 'Custom'],
+                        value_vars=['3%3mm', '2%3mm', '1%3mm'],
+                        var_name='Percentage', value_name='Value')
 
 # Set the theme
 sns.set_theme(style="white")
 
 # Set up the matplotlib figure with smaller size
-plt.figure(figsize=(10, 2.5))
+plt.figure(figsize=(20, 5))
 
-# Creating box plots with Blues color palette
-palette = sns.color_palette("Blues", 3)
+# Creating box plots with a custom color palette
+palette = {'Original': '#1f77b4', 'DPP': '#2ca02c', 'PR': '#17becf', 'PRI': '#ff7f0e'}
 y_limits = (75, data_melted['Value'].max() + 5)  # Set minimum value to 75 and adjust the max limit for clarity
 
-for i, percentage in enumerate(['3%, 3mm', '2%, 3mm', '1%, 3mm'], start=1):
+for i, percentage in enumerate(['3%3mm', '2%3mm', '1%3mm'], start=1):
     plt.subplot(1, 3, i)
-    sns.boxplot(x='Correction', y='Value', data=data_melted[data_melted['Percentage'] == percentage],
-                hue='Correction', palette=palette, dodge=False)
+    sns.boxplot(x='Custom', y='Value', data=data_melted[data_melted['Percentage'] == percentage], palette=palette)
     plt.title(f'{percentage}', fontsize=12)  # Increase title font size
     plt.ylim(y_limits)
     plt.xticks(rotation=45, fontsize=10)  # Increase x-tick labels font size
--- src/plotsforpresentation/summarystripplot.py
+++ src/plotsforpresentation/summarystripplot.py
@@ -3,13 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the data
-file_path = (r"P:\02_QA Equipment\02_ArcCheck\05_Commissoning\03_NROAC\Dose Rate Dependence Fix\Test on "
-             r"script\ACResultsPowerQuerySummary.txt")
+file_path = r"P:\02_QA Equipment\02_ArcCheck\05_Commissoning\03_NROAC\Dose Rate Dependence Fix\Test on script\ACResultsPowerQuerySummary.txt"
 data = pd.read_csv(file_path, sep='\t')
 
 # Melt the data
-data_melted = data.melt(id_vars=['Plans', 'Correction'],
-                        value_vars=['3%, 3mm', '2%, 3mm', '1%, 3mm'],
+data_melted = data.melt(id_vars=['Plan Name', '(mm)', 'Custom'],
+                        value_vars=['3%3mm', '2%3mm', '1%3mm'],
                         var_name='Percentage', value_name='Value')
 
 # Set the theme
@@ -19,19 +18,19 @@ sns.set_theme(style="whitegrid")
 plt.figure(figsize=(15, 5))
 
 # Creating strip plots with a custom color palette
-palette = {'none': '#1f77b4', 'DPP': '#2ca02c', 'PR': '#17becf'}
+palette = {'Original': '#1f77b4', 'DPP': '#2ca02c', 'PR': '#17becf', 'PRI': '#ff7f0e'}
 x_limits = (80, data_melted['Value'].max() + 5)  # Set minimum value to 80 and adjust the max limit for clarity
 
-for i, percentage in enumerate(['3%, 3mm', '2%, 3mm', '1%, 3mm'], start=1):
+for i, percentage in enumerate(['3%3mm', '2%3mm', '1%3mm'], start=1):
     plt.subplot(1, 3, i)
-    sns.stripplot(x='Value', y='Plans', data=data_melted[data_melted['Percentage'] == percentage],
-                  hue='Correction', palette=palette, dodge=True, jitter=True)
+    sns.stripplot(x='Value', y='Plan Name', data=data_melted[data_melted['Percentage'] == percentage],
+                  hue='Custom', palette=palette)
     plt.title(f'{percentage}', fontsize=12)  # Increase title font size
     plt.xlim(x_limits)
     plt.xticks(rotation=45, fontsize=10)  # Increase x-tick labels font size
     plt.yticks(fontsize=10)  # Increase y-tick labels font size
     plt.grid(axis='x')  # Add horizontal grid lines
-    for j in range(len(data['Plans'].unique())):
+    for j in range(len(data['Plan Name'].unique())):
         plt.axhline(y=j, color='gray', linestyle='--', linewidth=0.7)
     if i == 1:
         plt.xlabel('Pass Rates', fontsize=12)  # Set x-axis label
